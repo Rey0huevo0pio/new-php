cb7b230ea633abc5aed4cdbc17af27fa776f9a3e
Rey0huevo0pio <156557263+Rey0huevo0pio@users.noreply.github.com>
2024-03-23 13:37:28 -0600

    codigo php, html
    codigo php

---
+++ proyec/acer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="edicion.css">
+</head>
+<body>
+<div class="caja2">
+</section>
+    <img src="Carpeta_Imagen/loteria mexicana cartas pdf_page-002.jpg" class="imag">
+                <div class="caja1">
+                    <P CLASS="NOMBRE">REYES CORTES LEONEL</P>
+                </div>
+            <section>
+<h2 id="ti">ACERCA DE MI </h2>
+            <p>¡
+                Soy LEONEL REYES CORTES y estudio ingeneria de software que consiste en programar
+                inovar nuevas tecnologias estoy en mitad de mi carrea ya voy en medio camino ya falta la mitan de el
+                quiero terminar mis estudios y enpezar a trabajar en lo que yo quiero
+                en mi vida e tenidos muchas dificultades y conseguir trabajo sera una de ellas.
+            </p>
+</div>
+</body>
+</html>
